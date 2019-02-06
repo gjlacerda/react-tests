24c7f0cd761ac6f7a34c48b3448896e11597c413
Guilherme Lacerda <guilherme@Guilhermes-MacBook-Pro.local>
2019-02-06 14:22:02 -0200

    Add initial structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './dist',
+  }
+}
