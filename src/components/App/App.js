b560dbefc44b73cd4948cca7b295bdab143369e9
Guilherme Lacerda <guilherme@Guilhermes-MacBook-Pro.local>
2019-02-06 15:43:26 -0200

    Reorganize structure folder

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Counter from 'components/Counter'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Counter/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
